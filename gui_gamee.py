f332555e7323b20c8539f680c9701a6f962f2f46
Kendal Goevert <kgoevert@stcharlessd.org>
2020-02-13 12:12:40 -0600

    modified:   gui_gamee.py

--- gui_gamee.py
+++ gui_gamee.py
@@ -12,7 +12,7 @@ BUTTON_FONT = ("Arial", 15)
 
 ##MAIN
 
-class MainMenu(tk.Frame):
+'''class MainMenu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         self.lbl_title = tk.Label(self, text = "Game Library",  font = TITLE_FONT)
@@ -33,37 +33,59 @@ class MainMenu(tk.Frame):
         self.grid_columnconfigure(1, weight = 1)
         self.grid_columnconfigure(2, weight = 1)
         self.grid_columnconfigure(3, weight = 1)
-        self.grid_columnconfigure(4, weight = 1)
+        self.grid_columnconfigure(4, weight = 1)'''
         
 
 
-'''class Search(tk.Frame): 
+class Search(tk.Frame): 
     def __init__(self):
         tk.Frame.__init__(self)
-        self.lbl_title = tk.Label(text = "Search",  font = TITLE_FONT)
+        self.lbl_title = tk.Label(self,text = "Search",  font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0, sticky ="news")
         #Search bb label
-        self.lbl_search_by = tk.Label(text = "Search By", font = BUTTON_FONT)
-        self.lbl_search_by.grid(row = 1, column = 0, sticky ="news")
+        self.lbl_search_by = tk.Label(self, text = "Search By", font = BUTTON_FONT)
+        self.lbl_search_by.grid(row = 1, column = 0, sticky ="news")      
+       
         #Search by entry box
-        self.entry = tk.Entry (root)
-        self.entry.grid(row = 2, column = 0)
+        options = ["one", "two"]
+        self.tkvar = tk.StringVar(self)
+        self.tkvar.set(options[0])
+        self.menu = tk.OptionMenu(self,self.tkvar, *options )
+        self.menu.grid(row = 1, column = 1, sticky = "news")
+        
+        
+        
+        #self.ent = tk.Entry (root)
+        #self.ent.grid(row = 2, column = 0, sticky = "news")
         #Search for label
-        self.lbl_search_for = tk.Label(text = "Search For", font = BUTTON_FONT)
+        self.lbl_search_for = tk.Label(self,text = "Search For", font = BUTTON_FONT)
         self.lbl_search_for.grid(row = 3, column = 0, sticky ="news")
         #Search for entry box
-        self.entry = tk.Entry (root)
-        self.entry.grid(row = 4, column = 0)               
+        self.ent = tk.Entry (root)
+        self.ent.grid(row = 4, column = 0, sticky = "news")               
         #Scrolled Text Box
         self.results = ScrolledText(height = 8, width = 40)
-        self.results.grid(row = 5, columnspan = 3)
+        self.results.grid(row = 5, columnspan = 3, sticky = "news")
         #Back Clear and submit buttons
-        self.btn_add = tk.Button(text = "Back", font = BUTTON_FONT)
-        self.btn_add.grid(row = 6, column = 0)
-        self.btn_add = tk.Button(text = "Clear", font = BUTTON_FONT)
-        self.btn_add.grid(row = 6, column = 1)         
-        self.btn_add = tk.Button(text = "Submit", font = BUTTON_FONT)
-        self.btn_add.grid(row = 6, column = 2)'''
+        self.btn_add = tk.Button(self,text = "Back", font = BUTTON_FONT)
+        self.btn_add.grid(row = 6, column = 0, sticky = "news")
+        self.btn_add = tk.Button(self, text = "Clear", font = BUTTON_FONT)
+        self.btn_add.grid(row = 6, column = 1, sticky = "news")         
+        self.btn_add = tk.Button(self, text = "Submit", font = BUTTON_FONT)
+        self.btn_add.grid(row = 6, column = 2, sticky = "news")
+''' SubFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame__init__(self, master = parent)
+        
+        self.btn_one = tk.Button(self, text = "one")
+        self.btn_one.grid(row = 0, column = 0)
+        self.btn_two = tk.Button(self, text = "two")
+        self.btn_two.grid(row = 0, column = 1)   '''     
+        
+
+
+
+
 
 '''class Saved(tk.Frame):
     def __init__(self):
@@ -78,8 +100,8 @@ class MainMenu(tk.Frame):
         tk.Frame.__init__(self)
         self.lbl_title = tk.Label(text = "Which title would you like to edit?",  font = TITLE_FONT)
         self.lbl_title.grid(row = 0, columnspan = 5, sticky ="news")  
-        self.entry = tk.Entry (root)
-        self.entry.grid(row = 1, columnspan =4 )
+        self.ent = tk.Entry (root)
+        self.ent.grid(row = 1, columnspan =4 )
         self.btn_add = tk.Button(text = "Cancel", font = BUTTON_FONT)
         self.btn_add.grid(row = 2, column = 1)
         self.btn_add = tk.Button(text = "OK", font = BUTTON_FONT)
@@ -90,8 +112,8 @@ class MainMenu(tk.Frame):
         tk.Frame.__init__(self)
         self.lbl_title = tk.Label(text = "Which title would you like to remove?",  font = TITLE_FONT)
         self.lbl_title.grid(row = 0, columnspan = 5, sticky ="news")  
-        self.entry = tk.Entry (root)
-        self.entry.grid(row = 1, columnspan =4 )
+        self.ent = tk.Entry (root)
+        self.ent.grid(row = 1, columnspan =4 )
         self.btn_add = tk.Button(text = "Cancel", font = BUTTON_FONT)
         self.btn_add.grid(row = 2, column = 1)
         self.btn_add = tk.Button(text = "Remove", font = BUTTON_FONT)
@@ -110,7 +132,7 @@ class MainMenu(tk.Frame):
         self.btn_add = tk.Button(text = "Remove", font = BUTTON_FONT)
         self.btn_add.grid(row = 2, column = 2)   '''
 
-'''class Add(tk.Frame):
+class Add(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         self.lbl_title = tk.Label(text = "Add Game",  font = TITLE_FONT)
@@ -119,41 +141,51 @@ class MainMenu(tk.Frame):
         self.lbl_search_by = tk.Label(text = "Genre: ", font = BUTTON_FONT)
         self.lbl_search_by.grid(row = 1, column = 0, sticky ="news")        
         self.entry = tk.Entry (root)
-        self.entry.grid(row = 2, column = 0)
+        self.entry.grid(row = 1, column = 1)
         #Title entrybox
         self.lbl_search_by = tk.Label(text = "Title: ", font = BUTTON_FONT)
-        self.lbl_search_by.grid(row = 1, column = 1, sticky ="news")        
+        self.lbl_search_by.grid(row = 2, column = 0, sticky ="news")        
         self.entry = tk.Entry (root)
         self.entry.grid(row = 2, column = 1)
         #Decoloper entrybox
         self.lbl_search_by = tk.Label(text = "Devoloper: ", font = BUTTON_FONT)
         self.lbl_search_by.grid(row = 3, column = 0, sticky ="news")        
         self.entry = tk.Entry (root)
-        self.entry.grid(row = 4, column = 0)  
+        self.entry.grid(row = 3, column = 1)  
         #Publisher entrybox
         self.lbl_search_by = tk.Label(text = "Publisher: ", font = BUTTON_FONT)
-        self.lbl_search_by.grid(row = 3, column = 1, sticky ="news")        
+        self.lbl_search_by.grid(row = 4, column = 0, sticky ="news")        
         self.entry = tk.Entry (root)
         self.entry.grid(row = 4, column = 1)  
         #Year entrybox
         self.lbl_search_by = tk.Label(text = "Year: ", font = BUTTON_FONT)
         self.lbl_search_by.grid(row = 5, column = 0, sticky ="news")        
         self.entry = tk.Entry (root)
-        self.entry.grid(row = 6, column = 0)
+        self.entry.grid(row = 5, column = 1)
         #Notes Scrolled text box
         self.lbl_search_by = tk.Label(text = "Notes: ", font = BUTTON_FONT)
-        self.lbl_search_by.grid(row = 7, column = 0, sticky ="news")                
+        self.lbl_search_by.grid(row = 6, column = 0, sticky ="news")                
         self.results = ScrolledText(height = 8, width = 40)
-        self.results.grid(row = 8, columnspan = 3)
+        self.results.grid(row = 7, columnspan = 3)
         #Cancel Reset Remove Buttons
         self.btn_add = tk.Button(text = "Cancel", font = BUTTON_FONT)
-        self.btn_add.grid(row = 9, column = 1)
+        self.btn_add.grid(row = 9, column = 0, sticky ="news")
         self.btn_add = tk.Button(text = "Reset", font = BUTTON_FONT)
-        self.btn_add.grid(row = 9, column = 2)
+        self.btn_add.grid(row = 9, column = 1, sticky ="news")
         self.btn_add = tk.Button(text = "Confirm", font = BUTTON_FONT)
-        self.btn_add.grid(row = 9, column = 3)  '''       
+        self.btn_add.grid(row = 9, column = 2, sticky ="news")        
         
-        
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)
+        self.grid_columnconfigure(2, weight = 1)
+        self.grid_columnconfigure(3, weight = 1)
+        self.grid_columnconfigure(4, weight = 1)
+        self.grid_columnconfigure(5, weight = 1)
+        self.grid_columnconfigure(6, weight = 1)
+        self.grid_columnconfigure(7, weight = 1)
+        self.grid_columnconfigure(8, weight = 1)
+        self.grid_columnconfigure(9, weight = 1)
+        self.grid_columnconfigure(10, weight = 1)
     
     
       
@@ -173,8 +205,8 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("Game Lib")
     root.geometry("500x500")
-    mainmenu = MainMenu()
-    mainmenu.grid(row = 0, column = 0)
+    search = Search()
+    search.grid(row = 0, column = 0)
     root.grid_columnconfigure(0, weight = 1)
     
 
